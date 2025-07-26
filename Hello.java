0828ec3a46bea516cfb14e0465842e45f1f3da6e
Sushil Kumar <kumar595@yahoo.com>
2025-07-25 23:45:10 -0400

    Add 1 at end of message

--- Hello.java
+++ Hello.java
@@ -6,6 +6,6 @@ public class Hello {
     // Prints "Hello, Globe" to the terminal window.
     public static void main(String[] args)
     {
-        System.out.println("Hello, Globe Master");
+        System.out.println("Hello, Globe Master1");
     }
 }
