62a42d0fa465d471631d8f2060bdcb7c79532ce9
Sushil Kumar <kumar595@yahoo.com>
2025-07-26 00:14:17 -0400

    Added Try Catch Block to Hello.java

--- Hello.java
+++ Hello.java
@@ -4,8 +4,13 @@ public class Hello {
     
     // Your program begins with a call to main().
     // Prints "Hello, Globe" to the terminal window.
-    public static void main(String[] args)
-    {
+ public static void main(String[] args)
+{
+    try {
         System.out.println("Hello, Globe Master1");
+    } catch (Exception e) {
+        System.err.println("An error occurred: " + e.getMessage());
+        e.printStackTrace();
     }
+}  
 }
