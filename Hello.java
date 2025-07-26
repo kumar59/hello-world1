f2f7e5e69d00b557edbfe78d252a31df5788a2c8
kumar59 <kumar595@yahoo.com>
2025-07-25 23:33:44 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+// Simple Java program
+// FileName: "Hello.java"
+public class Hello {
+    
+    // Your program begins with a call to main().
+    // Prints "Hello, Globe" to the terminal window.
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Globe");
+    }
+}
